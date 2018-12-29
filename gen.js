949ed8108b80d75781b439e1558aeec1e957d320
jcrayton <jcrayton@macalester.edu>
2018-12-29 00:40:10 -0600

    somewhat started a random wall gen

--- gen.js
+++ gen.js
@@ -1,3 +1,7 @@
-function wallGen() {
-  return
+function randWallGen() {
+  setCell({x: getRandomInt(ROWS), y: getRandomInt(COLS)}, chars.wall)
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
